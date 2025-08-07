5d7f476badce95dbe45f1fed1ea7df59c431e5e4
a.magdi <menem.magdi222@gmail.com>
2025-08-07 13:41:10 +0300

    + Implement Splash Screen. + Adding Home Activity with Bottom APP Bar and FAB.

---
+++ app/src/main/java/com/example/todo_application/ui/SplashScreenActivity.kt
@@ -0,0 +1,23 @@
+package com.example.todo_application.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.example.todo_application.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed(
+                {
+                    startActivity(Intent(this, HomeActivity::class.java))
+                    finish()
+                }, 2500
+        )
+
+    }
+}
