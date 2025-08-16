9af203bf9f70f3cf94f53bfab56133c91e292ae9
Man3oom <menem.magdi222@gmail.com>
2025-08-16 17:12:09 +0300

    + enhancements

--- app/src/main/java/com/example/todo_application/adapters/ToDoFragmentRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/todo_application/adapters/ToDoFragmentRecyclerViewAdapter.kt
@@ -8,14 +8,15 @@ import com.example.todo_application.databinding.TaskCardBinding
 import com.example.todo_application.models.TodoCardData
 
 class ToDoFragmentRecyclerViewAdapter(private val items: List<TodoCardData>) : RecyclerView.Adapter<ToDoFragmentRecyclerViewAdapter.ViewHolder>() {
+   private lateinit var binding: TaskCardBinding
+
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): ViewHolder {
-        val view = TaskCardBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return ViewHolder(view)
+        binding = TaskCardBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
     }
-
     override fun onBindViewHolder(
         holder: ViewHolder,
         position: Int
