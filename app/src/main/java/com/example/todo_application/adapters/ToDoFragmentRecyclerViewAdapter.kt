44a270bd80fa40e9349e52ede302254cac362bbf
a.magdi <menem.magdi222@gmail.com>
2025-08-17 10:33:34 +0300

    + Enhancements

--- app/src/main/java/com/example/todo_application/adapters/ToDoFragmentRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/todo_application/adapters/ToDoFragmentRecyclerViewAdapter.kt
@@ -1,14 +1,17 @@
 package com.example.todo_application.adapters
 
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import androidx.viewbinding.ViewBinding
 import com.example.todo_application.databinding.TaskCardBinding
 import com.example.todo_application.models.TodoCardData
 
-class ToDoFragmentRecyclerViewAdapter(private val items: List<TodoCardData>) : RecyclerView.Adapter<ToDoFragmentRecyclerViewAdapter.ViewHolder>() {
-   private lateinit var binding: TaskCardBinding
+class ToDoFragmentRecyclerViewAdapter(private val items: List<TodoCardData>) :
+    RecyclerView.Adapter<ToDoFragmentRecyclerViewAdapter.ViewHolder>() {
+    private lateinit var binding: TaskCardBinding
+    private val TAG = "ToDoFragmentRecyclerViewAdapter"
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
@@ -17,19 +20,35 @@ class ToDoFragmentRecyclerViewAdapter(private val items: List<TodoCardData>) : R
         binding = TaskCardBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding)
     }
+
     override fun onBindViewHolder(
         holder: ViewHolder,
         position: Int
     ) {
         val item = items[position]
         holder.bind(item, position)
+
+        holder.itemBinding.RightMarkCard.setOnClickListener {
+            Log.d(TAG, "Right Mark Clicked")
+            items[position].isCompleted = true
+            notifyItemChanged(position)
+        }
     }
 
     override fun getItemCount(): Int = items.size
 
-    class ViewHolder(itemBinding: ViewBinding) : RecyclerView.ViewHolder(itemBinding.root){
-        fun bind(item: TodoCardData , position: Int) {
-            TODO("Not yet implemented")
+    class ViewHolder(val itemBinding: TaskCardBinding) : RecyclerView.ViewHolder(itemBinding.root) {
+        fun bind(item: TodoCardData, position: Int) {
+            itemBinding.TaskTitleTv.text = item.title
+            itemBinding.TaskTimeTv.text = item.time
+            if (item.isCompleted) {
+                itemBinding.RightMarkCard.visibility = View.INVISIBLE
+                itemBinding.DoneTextTv.visibility = View.VISIBLE
+            }
+            else{
+                itemBinding.RightMarkCard.visibility = View.VISIBLE
+                itemBinding.DoneTextTv.visibility = View.INVISIBLE
+            }
         }
     }
 }
--- app/src/main/java/com/example/todo_application/models/TodoCardData.kt
+++ app/src/main/java/com/example/todo_application/models/TodoCardData.kt
@@ -1,8 +1,8 @@
 package com.example.todo_application.models
 
 data class TodoCardData(
-    val title: String,
-    val description: String,
-    val time : String?,
-    val isCompleted: Boolean
+    var title: String,
+    var description: String,
+    var time : String?,
+    var isCompleted: Boolean
 )
